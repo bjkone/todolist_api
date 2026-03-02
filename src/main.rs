37b4e6ddd6c9dbdead1310522b1352be8a619e0c
bjkone <marcmakitu@hotmail.com>
2026-03-02 22:03:19 +0100

    handle exception

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{Json, Router, extract::{Path, State}, http::StatusCode, routing::{delete, get, post}};
+use axum::{Json, Router, extract::{Path, State}, http::{StatusCode, status}, response::IntoResponse, routing::{delete, get, post}};
 use serde::{Serialize, Deserialize};
 use std::{sync::{Arc, Mutex}};
 
@@ -32,17 +32,20 @@ async fn creer_tache(State(state): State<AppState>, Json(tache): Json<Tache>) ->
     StatusCode::CREATED
 }
 
-async fn supprimer_tache(State(state): State<AppState>, Path(nom): Path<String>) -> StatusCode {
+async fn supprimer_tache(State(state): State<AppState>, Path(nom): Path<String>) -> Result<StatusCode, ApiError> {
     let mut taches = state.taches.lock().unwrap();
 
     let existe = taches.iter().any(|t| t.name == nom);
 
     if !existe {
-        return StatusCode::NOT_FOUND;
+        return Err(ApiError::TacheNonTrouvee { 
+            message: String::from("taches n'existe pas")
+            }
+        );
     }
 
     taches.retain(|t| t.name != nom);
-    StatusCode::NO_CONTENT
+    Ok(StatusCode::NO_CONTENT)
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -62,4 +65,41 @@ enum Priorite {
 #[derive(Clone)]
 struct AppState {
     taches: Arc<Mutex<Vec<Tache>>>,
+}
+
+#[derive(Serialize)]
+struct ErrorJson {
+    message: String,
+}
+
+enum ApiError {
+    TacheNonTrouvee { message: String },
+    TacheInvalide {message: String},
+    TacheDejaExistante { message: String },
+}
+
+impl IntoResponse for ApiError  {
+    fn into_response(self) -> axum::response::Response {
+        let (status_code, error_json) = match self {
+            ApiError::TacheNonTrouvee { message } => {
+                (
+                    StatusCode::NOT_FOUND,
+                    ErrorJson { message },
+                )
+            }
+            ApiError::TacheInvalide { message } => {
+                (
+                    StatusCode::BAD_REQUEST,
+                    ErrorJson { message },
+                )
+            }
+            ApiError::TacheDejaExistante { message } => {
+                (
+                    StatusCode::CONFLICT,
+                    ErrorJson { message }
+                )
+            }
+        };
+        (status_code, Json(error_json)).into_response()
+    }
 }
